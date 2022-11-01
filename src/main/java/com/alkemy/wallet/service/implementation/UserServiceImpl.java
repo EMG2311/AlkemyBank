c58a0ba8b99f873ce884de0dc98462a485c03f3a
lditzel94 <lucianoditzel94@gmail.com>
2022-11-01 16:05:34 -0300

    user service and repository added

---
+++ src/main/java/com/alkemy/wallet/service/implementation/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.alkemy.wallet.service.implementation;
+
+import com.alkemy.wallet.dto.UserDto;
+import com.alkemy.wallet.dto.UserRequestDto;
+import com.alkemy.wallet.repository.UserRepository;
+import com.alkemy.wallet.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private UserRepository repository;
+
+    @Override
+    public UserDto createUser( UserRequestDto userRequestDto ) {
+        return null;
+    }
+}
