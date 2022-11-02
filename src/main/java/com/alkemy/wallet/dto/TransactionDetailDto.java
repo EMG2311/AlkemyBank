3bc24da6d4871f736069cc8586e2b1f34f99581f
Micael <micaelcapo1316@gmail.com>
2022-11-02 01:09:12 -0300

    feature/create_Transaction_Service: Added @Builder Anotation

--- src/main/java/com/alkemy/wallet/dto/TransactionDetailDto.java
+++ src/main/java/com/alkemy/wallet/dto/TransactionDetailDto.java
@@ -1,7 +1,9 @@
 package com.alkemy.wallet.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 
+@Builder
 public record TransactionDetailDto(
         @JsonProperty( "id" ) Integer id,
         @JsonProperty( "amount" ) Double amount,
