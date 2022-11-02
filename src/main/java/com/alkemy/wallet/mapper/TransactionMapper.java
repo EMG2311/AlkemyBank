97548208dd256d20326ed5f46aab845984a56482
Micael <micaelcapo1316@gmail.com>
2022-11-02 15:21:57 -0300

    feature/create_Transaction_Service: Created Transaction Mapper to convertDto

---
+++ src/main/java/com/alkemy/wallet/mapper/TransactionMapper.java
@@ -0,0 +1,21 @@
+package com.alkemy.wallet.mapper;
+
+import com.alkemy.wallet.dto.TransactionDetailDto;
+import com.alkemy.wallet.model.Transaction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TransactionMapper {
+
+//    private final ModelMapper mapper;
+//
+//    public TransactionDetailDto convertToDto(Transaction transaction){
+//        return mapper.map( transaction, TransactionDetailDto.class);
+//    }
+//
+//    public Transaction convertToEntity( TransactionDetailDto transactionDetailDto){
+//        return mapper.map( transactionDetailDto, Transaction.class);
+//    }
+}
