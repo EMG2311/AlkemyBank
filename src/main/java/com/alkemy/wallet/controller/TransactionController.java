e43143a451f6e34ed08338cdf78fdc768b07ef44
Micael <micaelcapo1316@gmail.com>
2022-11-02 12:20:06 -0300

    feature/create_Transaction_Service: Modified Controller, now has return ResponseEntity

--- src/main/java/com/alkemy/wallet/controller/TransactionController.java
+++ src/main/java/com/alkemy/wallet/controller/TransactionController.java
@@ -3,13 +3,12 @@ package com.alkemy.wallet.controller;
 import com.alkemy.wallet.model.Transaction;
 import com.alkemy.wallet.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/transactions")
@@ -19,8 +18,8 @@ public class TransactionController {
 
     @GetMapping( value = "/{id}")
     @PreAuthorize("hasRole('USER_ROLE')")
-    public Optional<Transaction> getTransactionDetailById(@PathVariable Integer id ) {
-        return transactionService.getTransactionDetailById(id);
+    public ResponseEntity<Transaction> getTransactionDetailById(@PathVariable("id") Integer id ) throws Exception {
+        return ResponseEntity.ok(transactionService.getTransactionDetailById(id));
     }
 
 }
