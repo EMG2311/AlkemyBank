a7a225ea2ddc66e3cfd5d825130eac758a3389cf
Micael <micaelcapo1316@gmail.com>
2022-11-03 16:30:46 -0300

    feature/create_Transaction_Service: Modified Service, now It does the convertion to Dto

--- src/main/java/com/alkemy/wallet/service/TransactionService.java
+++ src/main/java/com/alkemy/wallet/service/TransactionService.java
@@ -1,7 +1,6 @@
 package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.TransactionDetailDto;
-import com.alkemy.wallet.dto.TransactionDepositDto;
 import com.alkemy.wallet.model.Transaction;
 
 public interface TransactionService {
