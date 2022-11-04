46459165573ea657af4267984946adb32c95925c
alejandrodussan <jdussan@unicesar.edu.co>
2022-11-03 21:20:53 -0500

    Add method UpdateTransaction into TransactionService

--- src/main/java/com/alkemy/wallet/service/TransactionService.java
+++ src/main/java/com/alkemy/wallet/service/TransactionService.java
@@ -1,11 +1,14 @@
 package com.alkemy.wallet.service;
 
 import com.alkemy.wallet.dto.TransactionDetailDto;
+import com.alkemy.wallet.dto.TransactionPatchDto;
 import com.alkemy.wallet.model.Transaction;
 
 public interface TransactionService {
     TransactionDetailDto getTransactionDetailById (Integer Id) throws Exception;
 
     Transaction createDeposit(Transaction transaction);
+
+    TransactionDetailDto updateTransaction(TransactionPatchDto transaction, Integer Id) throws Exception;
 
 }
