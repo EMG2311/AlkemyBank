ac0dd706066bc8710039d8a48c0beda67fa6a1e3
Micael <micaelcapo1316@gmail.com>
2022-11-05 16:37:25 -0300

    feature/Endpoint_get_detail_of_a_user: Modified UserController, now has getUserDetailById and verifies that only a user can get it owns details

--- src/main/java/com/alkemy/wallet/controller/UserController.java
+++ src/main/java/com/alkemy/wallet/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.alkemy.wallet.controller;
 
+import com.alkemy.wallet.dto.UserDetailDto;
 import com.alkemy.wallet.dto.UserDto;
+import com.alkemy.wallet.model.User;
 import com.alkemy.wallet.service.UserService;
+import com.alkemy.wallet.service.implementation.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,10 +16,20 @@ import java.util.List;
 @RestController
 @RequestMapping( "/users" )
 class UserController {
-    private final UserService service;
+    private final UserService userservice;
+
+    private final UserServiceImpl userServiceImpl;
 
     @GetMapping
     List<UserDto> getAll() {
-        return service.getAllUsers();
+        return userservice.getAllUsers();
     }
+
+    @GetMapping( value = "/{id}")
+    @PreAuthorize("hasRole('USER_ROLE')")
+    public ResponseEntity<UserDetailDto> getUserDetailById(@PathVariable("id") Integer id, @RequestHeader("Authorization") String token ) throws Exception {
+       User user = userServiceImpl.getUser(id,token);
+       return ResponseEntity.ok(userServiceImpl.getUserDetailById(user.getUserId()));
+    }
+
 }
