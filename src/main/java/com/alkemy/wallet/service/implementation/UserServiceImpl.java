5779f974c851f3ac878f3d37dff47f742aeeaa84
Micael <micaelcapo1316@gmail.com>
2022-11-05 16:44:06 -0300

    feature/Endpoint_get_detail_of_a_user: Modified UserServiceImpl, has method to getUserDetailById and has User verification to allow a user only to get its own detail

--- src/main/java/com/alkemy/wallet/service/implementation/UserServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/implementation/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package com.alkemy.wallet.service.implementation;
 
+import com.alkemy.wallet.dto.UserDetailDto;
 import com.alkemy.wallet.dto.UserDto;
 import com.alkemy.wallet.dto.UserRequestDto;
+import com.alkemy.wallet.exception.ForbiddenAccessException;
+import com.alkemy.wallet.exception.ResourceNotFoundException;
 import com.alkemy.wallet.mapper.UserMapper;
 import com.alkemy.wallet.model.Role;
 import com.alkemy.wallet.model.RoleName;
 import com.alkemy.wallet.model.User;
 import com.alkemy.wallet.repository.UserRepository;
+import com.alkemy.wallet.security.JWTUtil;
 import com.alkemy.wallet.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -15,6 +19,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.Id;
 import javax.swing.text.html.parser.Entity;
 import java.sql.Timestamp;
 import java.util.List;
@@ -24,6 +29,8 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     private final UserRepository repository;
     private final UserMapper mapper;
+
+    private final JWTUtil jwtUtil;
 
     @Override
     public List<UserDto> getAllUsers() {
@@ -54,8 +61,15 @@ public class UserServiceImpl implements UserService {
         return entityToDTO(repository.save(user));
     }
 
-
-
+    @Override
+    public UserDetailDto getUserDetailById(Integer Id) {
+        var user = repository.findById(Id);
+        if(user.isPresent()){
+            return mapper.convertToDetailDto(user.get());
+        }else{
+            throw new ResourceNotFoundException("User does not exist");
+        }
+    }
 
     public UserDto entityToDTO(User user){
         //Mappers commented until they work automatically
@@ -82,5 +96,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public User loadUserByUsername(String email) throws UsernameNotFoundException {
         return repository.findByEmail(email);
+    }
+
+    public User getUser(Integer id, String token) throws ForbiddenAccessException {
+        String jwt;
+        jwt = token.substring(7);
+        String email = jwtUtil.extractUserName(jwt);
+        User user = loadUserByUsername(email);
+        if(user.getUserId().equals(id)){
+            return user;
+        }else{
+            throw new ForbiddenAccessException("Cannot access another user details");
+        }
     }
 }
