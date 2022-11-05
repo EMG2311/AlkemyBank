6e4f448eb4f287f793b6fde7a6fb24433472af5b
Mariano Chun <marianochun01@gmail.com>
2022-11-04 21:21:41 -0300

    Add Account attribute in TransactionDepositDto

--- src/main/java/com/alkemy/wallet/dto/TransactionDepositDto.java
+++ src/main/java/com/alkemy/wallet/dto/TransactionDepositDto.java
@@ -1,14 +1,17 @@
 package com.alkemy.wallet.dto;
 
+import com.alkemy.wallet.model.Account;
 import com.alkemy.wallet.model.TransactionType;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import java.sql.Timestamp;
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 public class TransactionDepositDto {
     @JsonProperty( "id" )
     private Integer transactionID;
@@ -25,6 +28,8 @@ public class TransactionDepositDto {
 
     @JsonProperty( "transactionDate" )
     private Timestamp transactionDate;
+
+    private Account account;
 
     public TransactionDepositDto(Double amount, String description) {
         this.amount = amount;
