85df4b10b53a4049953b7a4bfe4e2f6e35fae761
Manuel Pautasso <manuelpautasso@gmail.com>
2022-11-13 21:31:40 -0300

    Fix correct test to get balance

--- src/test/java/com/alkemy/wallet/controller/transaction/GetBalanceEndpointTests.java
+++ src/test/java/com/alkemy/wallet/controller/transaction/GetBalanceEndpointTests.java
@@ -7,11 +7,10 @@ import com.alkemy.wallet.mapper.AccountMapper;
 import com.alkemy.wallet.mapper.FixedTermDepositMapper;
 import com.alkemy.wallet.model.*;
 import com.alkemy.wallet.model.Currency;
+import com.alkemy.wallet.repository.AccountRepository;
 import com.alkemy.wallet.repository.FixedTermDepositRepository;
 import com.alkemy.wallet.repository.UserRepository;
 import com.alkemy.wallet.security.JWTUtil;
-import com.alkemy.wallet.service.AccountService;
-import com.alkemy.wallet.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,7 +26,6 @@ import java.util.*;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -37,20 +35,14 @@ public class GetBalanceEndpointTests {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private AccountService accountService;
-
-    @Autowired
-    private UserService userService;
+    @MockBean
+    private AccountRepository accountRepository;
 
     @MockBean
     private UserRepository userRepository;
 
     @MockBean
     private FixedTermDepositRepository fixedTermDepositRepository;
-
-    @MockBean
-    private JWTUtil jwtUtilMocked;
 
     @Autowired
     private JWTUtil jwtUtil;
@@ -73,7 +65,8 @@ public class GetBalanceEndpointTests {
     FixedTermDeposit fixedTermDepositArs1, fixedTermDepositArs2, fixedTermDepositUsd;
 
     AccountDto arsAccount, usdAccount;
-    
+    Account acc1, acc2;
+
     private List<FixedTermDepositDto> usdFixedTermDepositsDto, arsFixedTermDepositsDto;
 
     private List<AccountDto> userAccounts;
@@ -82,32 +75,34 @@ public class GetBalanceEndpointTests {
     void setUp() {
         Role userRole = new Role(RoleName.USER, "user", timestamp, timestamp);
 
-        user1 = new User(1, "Pepe", "Lopez", "lopez@example.com", "123", userRole, timestamp, timestamp, false);
+        user1 = new User(1, "Marcos", "Fernandez", "email@email", "kjkjkjw", userRole, timestamp, timestamp, false);
 
-//        when(userRepository.findByEmail(user1.getEmail())).thenReturn(user1);
-//        when(userRepository.findById(user1.getUserId())).thenReturn(Optional.of(user1));
-        //token1 = jwtUtil.generateToken(user1);
-
-        when(jwtUtilMocked.extractClaimUsername(token1.substring(7))).thenReturn(user1.getEmail());
-        when(userService.loadUserByUsername(user1.getEmail())).thenReturn(user1);
+        token1 = jwtUtil.generateToken(user1);
+        when(userRepository.findByEmail(user1.getEmail())).thenReturn(user1);
+        when(userRepository.findById(user1.getUserId())).thenReturn(Optional.of(user1));
 
 
-        arsAccount = new AccountDto(1,user1.getUserId(), 20000.0, Currency.ARS, 70000.0, timestamp, timestamp, false);
-        usdAccount = new AccountDto(2, user1.getUserId(), 8000.0, Currency.USD, 10000.0, timestamp, timestamp, false);
-        userAccounts = new ArrayList<>();
+        acc1 = new Account(1, Currency.ARS, 10000d, 75000d, user1, timestamp, timestamp, false);
+        acc2 = new Account(2, Currency.USD, 2000d, 35000d, user1, timestamp, timestamp, false);
+        when(accountRepository.findAccountsByUserId(user1)).thenReturn(List.of(acc1,acc2));
+
+        arsAccount = new AccountDto(1, user1.getUserId(), 75000d, Currency.ARS, 10000d, timestamp, timestamp, false);
+        usdAccount = new AccountDto(2, user1.getUserId(), 35000d, Currency.USD, 2000d, timestamp, timestamp, false);
+        /*userAccounts = new ArrayList<>();
         userAccounts.add(arsAccount);
-        userAccounts.add(usdAccount);
-        
-        when(accountService.getAccountsByUserId(user1.getUserId(), token1)).thenReturn(userAccounts);
+        userAccounts.add(usdAccount);*/
 
-        fixedTermDepositArs1 = new FixedTermDeposit(1, 1000.0, accountMapper.convertToEntity(arsAccount), 50.0, timestamp, timestamp);
-        fixedTermDepositArs2 = new FixedTermDeposit(1, 2000.0, accountMapper.convertToEntity(arsAccount), 50.0, timestamp, timestamp);
-        fixedTermDepositUsd = new FixedTermDeposit(1, 3000.0, accountMapper.convertToEntity(usdAccount), 5.0, timestamp, timestamp);
+        //when(accountService.getAccountsByUserId(user1.getUserId(), token1)).thenReturn(userAccounts);
 
+        fixedTermDepositArs1 = new FixedTermDeposit(1, 1000.0, acc1, 50.0, timestamp, timestamp);
+        fixedTermDepositArs2 = new FixedTermDeposit(2, 2000.0, acc1, 50.0, timestamp, timestamp);
+        fixedTermDepositUsd = new FixedTermDeposit(3, 3000.0, acc2, 5.0, timestamp, timestamp);
 
-        arsFixedTermDepositsDto = Arrays.asList(fixedTermDepositMapper.convertToDto(fixedTermDepositArs1), fixedTermDepositMapper.convertToDto(fixedTermDepositArs2));
+        when(fixedTermDepositRepository.findByAccount_AccountId(acc1.getAccountId())).thenReturn(List.of(fixedTermDepositArs1, fixedTermDepositArs2));
+        when(fixedTermDepositRepository.findByAccount_AccountId(acc2.getAccountId())).thenReturn(List.of(fixedTermDepositUsd));
+
+        arsFixedTermDepositsDto = List.of(fixedTermDepositMapper.convertToDto(fixedTermDepositArs1), fixedTermDepositMapper.convertToDto(fixedTermDepositArs2));
         usdFixedTermDepositsDto = List.of(fixedTermDepositMapper.convertToDto(fixedTermDepositUsd));
-
 
 
 //        when(fixedTermDepositRepository.findByAccount_AccountId(arsAccount.id())).thenReturn(arsFixedTermDeposits);
@@ -117,7 +112,7 @@ public class GetBalanceEndpointTests {
     }
 
     @Test
-    void getUserBalance() throws  Exception{
+    void getUserBalance() throws Exception {
 
         List<AccountBalanceDto> expectedListAccountBalanceDto = new LinkedList<>();
         AccountBalanceDto accountBalanceDtoArs = accountMapper.convertAccountDtoToAccountBalanceDto(arsAccount);
@@ -125,9 +120,6 @@ public class GetBalanceEndpointTests {
 
         accountBalanceDtoArs.setFixedTermDeposits(arsFixedTermDepositsDto);
         accountBalanceDtoUsd.setFixedTermDeposits(usdFixedTermDepositsDto);
-
-//        when(accountService.getAccountBalance(arsAccount)).thenReturn(accountBalanceDtoArs);
-//        when(accountService.getAccountBalance(usdAccount)).thenReturn(accountBalanceDtoUsd);
 
         expectedListAccountBalanceDto.add(accountBalanceDtoArs);
         expectedListAccountBalanceDto.add(accountBalanceDtoUsd);
@@ -141,8 +133,11 @@ public class GetBalanceEndpointTests {
                 .andExpect(jsonPath("$[1].id").value(accountBalanceDtoUsd.getId()))
                 .andExpect(jsonPath("$[0].balance").value(accountBalanceDtoArs.getBalance()))
                 .andExpect(jsonPath("$[1].balance").value(accountBalanceDtoUsd.getBalance()))
-                .andExpect(jsonPath("$[0].currency").value(accountBalanceDtoArs.getCurrency()))
-                .andExpect(jsonPath("$[1].currency").value(accountBalanceDtoUsd.getCurrency()))
+                .andExpect(jsonPath("$[0].currency").value(accountBalanceDtoArs.getCurrency().toString()))
+                .andExpect(jsonPath("$[1].currency").value(accountBalanceDtoUsd.getCurrency().toString()))
+                .andExpect(jsonPath("$[0].fixedTermDeposits[0].amount").value(accountBalanceDtoArs.getFixedTermDeposits().get(0).getAmount()))
+                .andExpect(jsonPath("$[0].fixedTermDeposits[1].amount").value(accountBalanceDtoArs.getFixedTermDeposits().get(1).getAmount()))
+                .andExpect(jsonPath("$[1].fixedTermDeposits[0].amount").value(accountBalanceDtoUsd.getFixedTermDeposits().get(0).getAmount()))
         ;
     }
 
